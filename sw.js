c23314f03357a6c0441186d859adb4bbafefd45f
Angel0973180707 <a0973180707@gmail.com>
2025-12-25 18:16:36 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* 影音秘書 SW (v1.4) */
+const CACHE = "video-secretary-v1-4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
